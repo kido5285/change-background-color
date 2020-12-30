d36d3d2cbb5bc8761d704a3e1f5f99ed31ea8f56
kido5285 <75468540+kido5285@users.noreply.github.com>
2020-12-30 14:12:03 +0800

    Update script.js

--- Background Image Slider(6)/script.js
+++ Background Image Slider(6)/script.js
@@ -8,6 +8,7 @@ next.addEventListener('click', nextBackground)
 prev.addEventListener('click', prevBackground)
 
 function nextBackground() {
+    container.animate([{opacity: 0.2},{opacity:1.0}], {duration: 1000, fill:'forwards'})
     img++;
     if(img === 5) {
         img = 0;
@@ -16,6 +17,7 @@ function nextBackground() {
 }
 
 function prevBackground() {
+    container.animate([{opacity: 0.2},{opacity:1.0}], {duration: 1000, fill:'forwards'})
     img--;
     if(img === -1) {
         img = 4;
